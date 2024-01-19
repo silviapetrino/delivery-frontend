3e9f9fcf80aff5e6f7274b02da3fe545bf51b60d
pietrolupi <pietro.lupi92@gmail.com>
2024-01-19 10:54:52 +0100

    [feat]: product quantity

--- src/pages/RestaurantDetail.vue
+++ src/pages/RestaurantDetail.vue
@@ -22,15 +22,30 @@ export default {
     getRestaurant(restaurant_id){
       axios.get(store.apiUrl + 'restaurants/restaurant-detail/' + restaurant_id).then(res=>{
         this.restaurant = res.data;
+
+        //aggiungo dinamicamente una nuova proprietà tempQuantity all'oggetto "product".
+        this.restaurant.products.forEach(product => {
+          product.tempQuantity = 1;
+        });
         this.isLoading = false;
-        console.log(res.data);
       })
     },
      
-    addToCart(product){
-      store.cart.push(product);
+    addToCart(product, quantity){
       
-      this.saveCart();
+      //"ciclo" tutti i prodotti già nel carrello guardando se ne trovo uno che abbia l'id uguale al prodotto che sto cercando di aggiungere (se non ne trovo qui mi risulta UNDEFINED)
+      let existingProduct = store.cart.find(productInCart => productInCart.id === product.id);
+
+      //SE let existingProduct esiste: aggiungo alla quantità del prodotto nel carrello la quantity che passo come parametro in addToCart()
+      if (existingProduct) {
+        existingProduct.quantity += parseInt(quantity);
+      } else {  //SE è undefined pusho il prodotto aggiungendo all'oggetto la proprietà "quantity" = quantità che passo come parametro nella function
+        store.cart.push({ ...product, quantity: parseInt(quantity) });
+      }
+      
+      this.saveCart(); 
+      
+      product.tempQuantity = 1;  //resetto la tempQuantity per scopi grafici nell'input DOPO aver salvato
     },
     
     removeFromCart(index){
@@ -82,17 +97,18 @@ export default {
         <h1 class="text-center mb-5">{{restaurant.name}}</h1>
     
         <div class="d-flex gap-5 flex-wrap justify-content-center">
-          <div v-for="(product) in restaurant.products" :key="product.id">
+          <div v-for="product in restaurant.products" :key="product.id">
     
             <div v-if="product.visibility == 1"  class="card" style="width: 18rem;">
-      
+          
               <img class="card-img-top" :src="product.image" alt="Card image cap">
               <div class="card-body">
                 <h5 class="card-title">{{product.name}} </h5>
                 <span>{{ product.price }} &euro; </span>
                 <p class="card-text">{{product.description}}</p>
                 <p>Ingredients: {{ product.ingredients }}</p>
-                <button @click="addToCart(product)" class="btn btn-primary">Add to cart</button>
+                <input type="number" v-model.number="product.tempQuantity" min="1" class="form-control mb-2" placeholder="Quantity">
+                <button @click="addToCart(product, product.tempQuantity)" class="btn btn-success">Add to cart</button>
                 
               </div>
             </div>
