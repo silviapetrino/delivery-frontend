c2cd9a739db32a531b4583db5aacb1e19af28a2e
silviapetrino <petrino.silvia@gmail.com>
2024-01-30 17:24:21 +0100

    [edit]: card menu

--- src/pages/RestaurantDetail.vue
+++ src/pages/RestaurantDetail.vue
@@ -36,7 +36,7 @@ export default {
     addToCart(product, quantity) {
       if (store.cart.length >= 0) {
         this.showToast[product.id] = true;
-        this.message[product.id] = `You added ${quantity} ${product.name} to the cart!`;
+        this.message[product.id] = `${quantity} ${product.name}`;
         product.tempQuantity = 1;
       }
 
@@ -163,17 +163,20 @@ export default {
 
         <h2 class="text-center mb-5">Menu</h2>
 
-        <div class="d-flex flex-column">
-          <div v-for="product in restaurant.products" :key="product.id" class="w-100">
+        <div class="container d-flex flex-column flex-md-row flex-md-wrap justify-content-center align-items-center w-100">
+
+          <div class="col-lg-6 d-flex justify-content-center align-items-center" v-for="product in restaurant.products" :key="product.id" >
+            
             <div v-if="product.visibility == 1"  class="card cs-card flex-row p-3 mb-3" >
               <div class="info-product d-flex flex-column w-75">
                 <h4 class="card-title">{{product.name}} </h4>
                 <p>Ingredients: {{ product.ingredients }}</p> 
                 <span>Price: {{ product.price }} &euro; </span>
 
-              <div  class="actions-and-toast d-flex gap-2">
-                <div style="width: 150px; height: 150px;" class="actions d-flex flex-column my-3">
-                  <div class="mb-2">
+              <div class="actions-and-toast d-flex gap-2 debug w-100 h-100">
+
+                <div style="width: 140px;" class="h-100 actions d-flex flex-column my-3">
+                  <div class="mb-1">
                     <button
                       @click="decreaseQuantity(product)"
                       class="btn btn-danger p-2 fw-bold rounded-1"
@@ -189,22 +192,22 @@ export default {
                     Add to cart
                   </button>
                 </div>
+
                     <!-- toast  -->
-                <div style="width: 200px;" v-if="showToast[product.id]" class="toast-cs text-center d-flex align-items-center justify-content-center " role="alert" aria-live="assertive" aria-atomic="true">
-                  <div class="toast-header d-flex flex-column">
-                    
-                    <img style="width: 80px;" src="/img/cart.jpg" class="rounded me-2" alt="add-to-cart">
-                  </div>
-                  <div class="toast-body message">
-                    <p class="message-title"><strong>Added to cart!</strong></p>
-                    <strong><span>{{ message[product.id] }}</span></strong>
+                <div style="width: 150px; " v-if="showToast[product.id]" class="toast-cs text-center d-flex align-items-center justify-content-center p-3" role="alert" aria-live="assertive" aria-atomic="true">
+                  <div class="toast-body">
+                      
+                    <strong>
+                      <span>You added {{ message[product.id] }} to cart</span>
+                      <img class="w-100 h-100 object-fit-cover" src="/img/cart-2.png" alt="cart">
+                    </strong>
                   </div>
                 </div>
               <!-- /toast  -->
               </div>
                 
               </div>
-              <div class="w-25 d-flex flex-column align-items-center">
+              <div class="image-container d-flex flex-column align-items-center">
                 <img :src="product.image" alt="Card image cap">
               </div>
             </div>
@@ -251,13 +254,34 @@ export default {
     }
   }
   .card.cs-card {
+    width: 400px;
+    height: 250px;
+    padding: 5px;
+    margin: 5px;
     box-shadow: 1px 3px 8px;
     color: $secondary_color;
+    position: relative;
     .add-product {
       width: 100px;
     }
-    img {
-      width: 75%;
+    @media all and (min-width: 576px){
+      width: 450px;
+    }
+    @media all and (min-width: 782px){
+      width: 800px;
+    }
+    @media all and (min-width: 1200px){
+      width: 550px;
+    }
+    .image-container {
+      width: 300px;
+     
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center;
+      }
     }
     .info{
       height: 70px;
@@ -265,17 +289,21 @@ export default {
     }
   }
 
+
   //  /modal 
 
-  // toast 
+
   .toast-cs {
     border: 3px solid $tertiary_color;
+    background-color:  rgba($color: #F5EBDC, $alpha: .8);
     border-radius: 20px;
-    // width: 250px;
     -webkit-box-shadow: -13px 8px 16px -16px rgb(248, 6, 6);
     -moz-box-shadow:  -13px 8px 16px -16px rgba(120, 82, 17, 1);
     box-shadow:  -13px 8px 16px -16px rgb(247, 194, 21);
-  
+    position: absolute;
+    top: 47%;
+    left: 47%;
+    transform: translate(-50%, -50%);
      .message-title{
       color:$secondary_color;
      }
@@ -336,5 +364,7 @@ export default {
     display: inline-block;
     text-align: center;
   }
+
+
 
 </style>
